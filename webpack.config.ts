a11ff1b6750f479c9839640c29646dc6f64b42a2
usagi-f <usagi.f@gmail.com>
2019-02-06 17:18:00 +0900

    update to use webpack

---
+++ webpack.config.ts
@@ -0,0 +1,57 @@
+import * as webpack from 'webpack';
+import * as path from 'path';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import { ColorsWithHash, Spacing } from './src/variables/';
+
+const config: webpack.Configuration = {
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    libraryTarget: 'commonjs2',
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: [
+                require('postcss-css-variables')({
+                  variables: Object.assign(ColorsWithHash, Spacing),
+                }),
+              ],
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: './css/all.css',
+      chunkFilename: '[id].css',
+    }),
+  ],
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx'],
+  },
+};
+
+export default config;
